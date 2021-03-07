4d531886b683b6753d184acc5073f8e275663713
wddnguyen <wddnguyen@gmail.com>
2021-03-07 15:27:55 -0500

    Added socket event handler for chess state and move

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ const createChessRoom = (roomName, side, roomOwner) => {
     }
 
     return {
-        roomName: roomName,
-        roomOwner: roomOwner,
+        roomName,
+        roomOwner,
         chessGame
     }
 }
@@ -37,6 +37,20 @@ const createChessGame = () => {
     }
 }
 
+const getChessStatus = (chess) => {
+    const fen = chess.fen()
+    const turn = chess.turn()
+    const history = chess.history()
+    const potentialMoves = chess.moves({verbose: true})
+
+    return {
+        fen,
+        turn,
+        history,
+        potentialMoves
+    }
+}
+
 // Socket handling
 io.on('connection', (socket) => {
     socket.on('set_socket_nickname', nickname => {
@@ -45,7 +59,8 @@ io.on('connection', (socket) => {
        
     console.log('A user is connected')
     socket.on('disconnect', () => {
-        console.log('user Disconnect')
+        console.log('user Disconnect', socket)
+        
     })
 
     socket.on('available_rooms', () => {
@@ -63,17 +78,17 @@ io.on('connection', (socket) => {
         let chessRoom = createChessRoom(newRoomId, newRoomInfo.side, user)
 
         roomMap.set(newRoomId, chessRoom)
-        //console.log('Create new room : ', newRoomId, socket.rooms, roomMap)
+        console.log('Create new room : ', newRoomId, socket.rooms, roomMap)
+        socket.emit('create_room_accepted', {newRoomId})
         io.emit('available_rooms', Array.from(roomMap.values()))
     })
 
-    socket.on('join_room', ({room, user}) => {
-        const roomName = room.roomName
+    socket.on('join_room', ({roomName, user}) => {
         const chessRoom = roomMap.get(roomName)
         
         if (chessRoom) {
             socket.join(roomName)
-            socket.emit('join_room_accepted')
+            socket.emit('join_room_accepted', {roomName})
 
             const room = io.sockets.adapter.rooms.get(roomName.toString())
             if (room.size === 2) {
@@ -90,9 +105,29 @@ io.on('connection', (socket) => {
             console.log('ERROR room not found')
         }
     })
+
+    socket.on('chess_state', ({roomId}) => {
+        console.log('CHESS STATE roomID', roomId)
+        const chessRoom = roomMap.get(roomId)
+        console.log(chessRoom)
+        const chessStatus = getChessStatus(chessRoom.chessGame.chess)
+        socket.emit('chess_state', chessStatus)
+    })
+
+    socket.on('move', ({roomId, from, to}) => {
+        console.log("CHESS MOVE roomID", roomId)
+        const chessRoom = roomMap.get(roomId)
+        const validMove = chessRoom.chessGame.chess.move({from: from, to: to})
+
+        if (validMove) {
+            const chessStatus = getChessStatus(chessRoom.chessGame.chess)
+            io.to(roomId).emit('chess_state', chessStatus)
+        }
+    })
 })
 
 const PORT = 3001
 const server = http.listen(PORT, () => {
     console.log('Server is running on port', server.address().port)
 })
+
