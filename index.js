1fc18cae7c3299246cd3e7255ee15dcd75731ed8
wddnguyen <wddnguyen@gmail.com>
2021-03-06 22:10:03 -0500

    Create new chess room when client creates game

--- index.js
+++ index.js
@@ -10,10 +10,39 @@ const io = require('socket.io')(http, {
 })
 
 let roomMap = new Map()
-let chess = new Chess()
-const NANO_ID_SIZE = 5
+const ROOM_NANO_ID_SIZE = 5
+const GAME_NANO_ID_SIZE = 5
 
+const createChessRoom = (roomName, side, roomOwner) => {
+    let chessGame = createChessGame()
+
+    if (side === 'white') {
+        chessGame.whitePlayer = roomOwner
+    } else {
+        chessGame.blackPlayer = roomOwner
+    }
+
+    return {
+        roomName: roomName,
+        roomOwner: roomOwner,
+        chessGame
+    }
+}
+const createChessGame = () => {
+    return {
+        whitePlayer: {},
+        blackPlayer: {},
+        gameId: nanoid(GAME_NANO_ID_SIZE),
+        chess: new Chess()
+    }
+}
+
+// Socket handling
 io.on('connection', (socket) => {
+    socket.on('set_socket_nickname', nickname => {
+        socket.nickname = nickname
+    }) 
+       
     console.log('A user is connected')
     socket.on('disconnect', () => {
         console.log('user Disconnect')
@@ -23,17 +52,27 @@ io.on('connection', (socket) => {
         socket.emit('available_rooms', Array.from(roomMap.values()))
     })
 
-    socket.on('create_room', newRoomInfo => {
-        const newRoom = {
-            id: nanoid(NANO_ID_SIZE),
-            roomOwner: newRoomInfo.roomOwner,
-            roomName: newRoomInfo.roomName,
-            ownerSide: newRoomInfo.side
+    socket.on('create_room', ({newRoomInfo, user}) => {
+        const newRoomId = nanoid(ROOM_NANO_ID_SIZE)
+        if (socket.rooms.has(newRoomId)) {
+            newRoomId = nanoid(ROOM_NANO_ID_SIZE)
         }
 
-        roomMap.set(newRoom.id, newRoom)
+        socket.join(newRoomId)     
 
+        let chessRoom = createChessRoom(newRoomId, newRoomInfo.side, user)
+
+        roomMap.set(newRoomId, chessRoom)
+        console.log('Create new room : ', newRoomId, socket.rooms, roomMap)
         socket.emit('available_rooms', Array.from(roomMap.values()))
+    })
+
+    socket.on('join_room', roomId => {
+        if (roomMap.get(roomId)) {
+            socket.emit('')
+        } else {
+            console.log('ERROR room not found')
+        }
     })
 })
 
