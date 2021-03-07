5227a559a97e84186d0299023c3c10918a143cd8
wddnguyen <wddnguyen@gmail.com>
2021-03-07 17:32:21 -0500

    Added socket handling for gameLog

--- index.js
+++ index.js
@@ -22,9 +22,12 @@ const createChessRoom = (roomName, side, roomOwner) => {
         chessGame.blackPlayer = roomOwner
     }
 
+    const gameLog = [`${roomOwner.userName} has joined.`]
+
     return {
         roomName,
         roomOwner,
+        gameLog,
         chessGame
     }
 }
@@ -33,6 +36,7 @@ const createChessGame = () => {
         whitePlayer: {},
         blackPlayer: {},
         gameId: nanoid(GAME_NANO_ID_SIZE),
+
         chess: new Chess()
     }
 }
@@ -63,12 +67,15 @@ io.on('connection', (socket) => {
         
     })
 
+    // Lobby Socket Handling
     socket.on('available_rooms', () => {
         socket.emit('available_rooms', Array.from(roomMap.values()))
     })
 
     socket.on('create_room', ({newRoomInfo, user}) => {
         const newRoomId = nanoid(ROOM_NANO_ID_SIZE)
+
+        // Generate another roomID if collision happened
         if (socket.rooms.has(newRoomId)) {
             newRoomId = nanoid(ROOM_NANO_ID_SIZE)
         }
@@ -78,7 +85,6 @@ io.on('connection', (socket) => {
         let chessRoom = createChessRoom(newRoomId, newRoomInfo.side, user)
 
         roomMap.set(newRoomId, chessRoom)
-        console.log('Create new room : ', newRoomId, socket.rooms, roomMap)
         socket.emit('create_room_accepted', {newRoomId})
         io.emit('available_rooms', Array.from(roomMap.values()))
     })
@@ -88,6 +94,7 @@ io.on('connection', (socket) => {
         
         if (chessRoom) {
             socket.join(roomName)
+            chessRoom.gameLog.push(`${user.userName} has joined`)
             socket.emit('join_room_accepted', {roomName})
 
             const room = io.sockets.adapter.rooms.get(roomName.toString())
@@ -106,16 +113,31 @@ io.on('connection', (socket) => {
         }
     })
 
-    socket.on('chess_state', ({roomId}) => {
-        console.log('CHESS STATE roomID', roomId)
+    // GameLog Socket Handling
+    socket.on('request_game_log', ({roomId}) => {
         const chessRoom = roomMap.get(roomId)
-        console.log(chessRoom)
+        if (chessRoom) {
+            socket.emit('game_log', {gameLog : chessRoom.gameLog})
+        }
+    })
+    socket.on('message', ({roomId, user, message}) => {
+        const chessRoom = roomMap.get(roomId)
+        if (chessRoom) {
+            if (message) {
+                chessRoom.gameLog.push(`${user.userName}: ${message}`)
+            }
+            io.to(roomId).emit('game_log', {gameLog : chessRoom.gameLog})
+        }
+    })
+
+    // Chess Game Socket Handling
+    socket.on('chess_state', ({roomId}) => {
+        const chessRoom = roomMap.get(roomId)
         const chessStatus = getChessStatus(chessRoom.chessGame.chess)
         socket.emit('chess_state', chessStatus)
     })
 
     socket.on('move', ({roomId, from, to}) => {
-        console.log("CHESS MOVE roomID", roomId)
         const chessRoom = roomMap.get(roomId)
         const validMove = chessRoom.chessGame.chess.move({from: from, to: to})
 
